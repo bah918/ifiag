f56e79ebd4955a3f1415bf7da13d288c8325a158
BAH <nafiou6@yahoo.fr>
2021-07-19 10:08:36 +0200

    ajout antd

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,16 @@
 //import React, {useEffect, useState} from 'react';
 import './App.css';
 import Navbar from './components/Navbar'
+//import { Button } from 'antd';
+import 'antd/dist/antd.css'
+
 
 
 function App() {
   
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <Navbar> </Navbar>
     </div>
   ); 
 }
