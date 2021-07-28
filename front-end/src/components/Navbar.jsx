568ec8c72a70cdec9963eed26772a8c99a3984f7
BAH <nafiou6@yahoo.fr>
2021-07-28 16:25:59 +0200

    Navbar

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-//import React, {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import Home from "../views/Home";
 import Contact from "../views/Contact";
@@ -8,12 +8,26 @@ import Signup from "../views/Signup";
 import Profil from "../views/Profil";
 
 function Navbar() {
+
+    const [connected, setConnected] = useState(false);
+    console.log(connected);
+  
+    useEffect(() => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        setConnected(true);
+        console.log(connected);
+      }
+    });
+
+
+
     return (
         <BrowserRouter>
             <nav className="navbar sticky-top navbar-expand-lg navbar-white bg-white">
-                <div  style={{backgroundImage: "linear-gradient(to bottom, #ffffff 2%, #D6EAF8)"}} className="container-fluid">
+                <div style={{ backgroundImage: "linear-gradient(to bottom, #ffffff 2%, #D6EAF8)" }} className="container-fluid">
                     <Link to="/" className="navbar-brand">
-                        <img style={{ width: "25%", marginRight: 623}}
+                        <img style={{ width: "25%", marginRight: 623 }}
                             src="./img/IFIAG - 5a - 30.10.2019 - LOGO.PNG"
                             alt="ifiag"
                         />
@@ -31,51 +45,50 @@ function Navbar() {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav">
-                                <>
-                                    <li className="nav-item">
-                                        <Link to="/Signup" className={`nav-link`} style={{color:'#2E86C1', fontWeight: "bolder"}}>
-                                            Inscription
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/login" className={`nav-link`}style={{color:'#2E86C1', fontWeight: "bolder"}}>
-                                            Connection
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/formations" className={`nav-link`} style={{color:'#2E86C1', fontWeight: "bolder"}}>
-                                            Formations
-                                        </Link>
-                                    </li>
-                                </>
-                            
-                                <>
-                                    <li className="nav-item">
-                                        <Link
-                                            to="/login"
-                                            onClick={() => {
-                                                return localStorage.removeItem("token");
-                                            }}
-                                            className={`nav-link`} style={{color:'#2E86C1', fontWeight: "bolder"}} >
-                                            Déconnexion
-                                        </Link>
-                                    </li>
-                            
-                                    <li className="nav-item">
-                                        <Link to="/profile" className={`nav-link`} 
-                                        style={{color:'#2E86C1', fontWeight: "bolder"}}>
-                                            Profil
-                                        </Link>
-                                    </li>
-                                    
-                                    <li className="nav-item">
-                                        <Link to="/contact" className={`nav-link`}
-                                        style={{color:'#2E86C1', fontWeight: "bolder"}}>
-                                            Contact
-                                        </Link>
-                                    </li>
-                                </>
-                        
+                            <>
+                                <li className="nav-item">
+                                    <Link to="/Signup" className={`nav-link`} style={{ color: '#2E86C1', fontWeight: "bolder" }}>
+                                        Inscription
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/login" className={`nav-link`} style={{ color: '#2E86C1', fontWeight: "bolder" }}>
+                                        Connection
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/formations" className={`nav-link`} style={{ color: '#2E86C1', fontWeight: "bolder" }}>
+                                        Formations
+                                    </Link>
+                                </li>
+                            </>
+                            <>
+                                <li className="nav-item">
+                                    <Link
+                                        to="/login"
+                                        onClick={() => {
+                                            return localStorage.removeItem("token");
+                                        }}
+                                        className={`nav-link`} style={{ color: '#2E86C1', fontWeight: "bolder" }} >
+                                        Déconnexion
+                                    </Link>
+                                </li>
+
+                                <li className="nav-item">
+                                    <Link to="/profile" className={`nav-link`}
+                                        style={{ color: '#2E86C1', fontWeight: "bolder" }}>
+                                        Profil
+                                    </Link>
+                                </li>
+
+                                <li className="nav-item">
+                                    <Link to="/contact" className={`nav-link`}
+                                        style={{ color: '#2E86C1', fontWeight: "bolder" }}>
+                                        Contact
+                                    </Link>
+                                </li>
+                            </>
+
                         </ul>
                     </div>
                 </div>
